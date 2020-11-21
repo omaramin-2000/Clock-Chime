3fe846902a562749f6badd181de1f1c65a3c6272
omaramin-2000 <65174210+omaramin-2000@users.noreply.github.com>
2020-11-21 20:04:55 +0200

    Update Clock-Chime.py

--- Clock-Chime.py
+++ Clock-Chime.py
@@ -14,22 +14,22 @@ print("Running...")
 while True:
     print(strftime("%H:%M:%S"), end="", flush=x)
     print("\r", end="", flush=x)
-    myHour = datetime.datetime.now().hour
-    myMinute = datetime.datetime.now().minute
-    mySecond = datetime.datetime.now().second
-    if myHour > 12: myHour = myHour - 12 # subtracts 12 from the 24 hours if after12 pm, or you will get 13,14,15 chimes, and so on
-    if myHour == 00: myHour = 12 # 12 am is 12 chimes
+    Hr = datetime.datetime.now().hour
+    Min = datetime.datetime.now().minute
+    Sec = datetime.datetime.now().second
+    if Hr > 12: Hr = Hr - 12 # subtracts 12 from the 24 hours if after12 pm, or you will get 13,14,15 chimes, and so on
+    if Hr == 00: Hr = 12 # 12 am is 12 chimes
     # only chimes if in the first 10 seconds of a minute, so this avoids a repeated chiming bug if the chime 
     # ends before the minute of 00, 15, 30, 45 passes
-    if 0 <= mySecond < 5: 
-        if myMinute == 00:
+    if 0 <= Sec < 5: 
+        if Min == 00:
             playsound('Hour Chime.mp3')
-            for y in range(myHour):
+            for y in range(Hr):
                 playsound('Hour Strike.mp3')
-        if x == 15 and myMinute == 15:
+        if x == 15 and Min == 15:
             playsound('Quarter Hour Chime.mp3')
-        if x in [15, 30] and myMinute == 30:
+        if x in [15, 30] and Min == 30:
             playsound('Half Hour Chime.mp3')
-        if x == 15 and myMinute == 45:
+        if x == 15 and Min == 45:
             playsound('3 Quarter Hour Chime.mp3')
     time.sleep(1) # runs the look once a second only to save CPU and battery
